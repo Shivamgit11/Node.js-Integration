7b803c0280ee830c3ac5f008f2d0c17e13e8a3a9
ShivamRauniyaar <145185374+ShivamRauniyaar@users.noreply.github.com>
2026-06-07 18:13:36 +0530

    fixing bugs

--- app.js
+++ app.js
@@ -24,12 +24,12 @@ const server = http.createServer((req, res) => {
     req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
-      console.log(message)
+      console.log(message);
       fs.writeFileSync("message.txt", message);
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
     });
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
