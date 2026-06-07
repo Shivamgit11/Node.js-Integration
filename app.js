2693ac8f0c998174a5d23c2fad227e1c5fac6659
ShivamRauniyaar <145185374+ShivamRauniyaar@users.noreply.github.com>
2026-06-07 16:47:10 +0530

    returning html based on the route

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 //http, https,fs,path,os
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer((req,res) => {
-    console.log(req.url, req.method, req.headers)
-    //process.exit()
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<html>')
-    res.write('<head><title>My First Page</title></head>')
-    res.write('<body><h1>Welcome to my Node Js project</h1></body>')
-    res.write('</html>')
-    res.end()
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  //process.exit()
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  if (req.url === "/home") {
+    res.write("<body><h1>Welcome to Home</h1></body>");
+  } else if (req.url === "/about") {
+    res.write("<body><h1>Welcome to About page</h1></body>");
+  } else if (req.url === "/about")  {
+    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+  } else {
+    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+  }
+  res.write("</html>");
+  res.end();
 });
 
-server.listen(4000)
+server.listen(4000);
