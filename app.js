affa2948101dbc0882616a2f01d0caee4c5751dc
ShivamRauniyaar <145185374+ShivamRauniyaar@users.noreply.github.com>
2026-06-07 17:59:58 +0530

    working with message storing with sections

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
 //http, https,fs,path,os
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  //process.exit()
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Submit</button></form></body>',
+    );
+    res.write("</html");
+    return res.end();
+  }
+
+  if (url === "/message" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      console.log(message)
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
-  if (req.url === "/home") {
-    res.write("<body><h1>Welcome to Home</h1></body>");
-  } else if (req.url === "/about") {
-    res.write("<body><h1>Welcome to About page</h1></body>");
-  } else if (req.url === "/about")  {
-    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
-  } else {
-    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
-  }
-  res.write("</html>");
+  res.write("<body><h1>Hello from Node.js Server!</h1></body>");
+  res.write("</html");
   res.end();
 });
 
