865cbd6eb25fa85c806cf47cdb266d2a61bb0d62
ShivamRauniyaar <145185374+ShivamRauniyaar@users.noreply.github.com>
2026-06-07 16:42:28 +0530

    returning the html section

--- app.js
+++ app.js
@@ -2,8 +2,14 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-    console.log(res)//
-    process.exit()
+    console.log(req.url, req.method, req.headers)
+    //process.exit()
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<head><title>My First Page</title></head>')
+    res.write('<body><h1>Welcome to my Node Js project</h1></body>')
+    res.write('</html>')
+    res.end()
 });
 
 server.listen(4000)
